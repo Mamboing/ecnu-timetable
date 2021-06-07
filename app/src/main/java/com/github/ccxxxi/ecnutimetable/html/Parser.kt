28aefb7a7368e684187a37ab6f9323991d66cd6a
CCXXXI <ccxxxi@qq.com>
2021-06-08 02:29:39 +0800

    临时用服务器解析数据

--- app/src/main/java/com/github/ccxxxi/ecnutimetable/html/Parser.kt
+++ app/src/main/java/com/github/ccxxxi/ecnutimetable/html/Parser.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import com.github.ccxxxi.ecnutimetable.ecnudb.Error
 import com.github.ccxxxi.ecnutimetable.ecnudb.LoginResult
 import com.github.ccxxxi.ecnutimetable.ecnudb.Success
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.jsoup.Jsoup
 
 object Parser {
@@ -24,6 +27,13 @@ object Parser {
         val doc = Jsoup.parse(html)!!
         val jsCode = doc.select("script[language=\"JavaScript\"]")!!.last().html()
 
-        TODO("从那堆代码里提取课表数据")
+        // todo: 不借助服务器，直接在本地解析数据
+        val body = FormBody.Builder().add("code", jsCode).build()
+        val request = Request.Builder().url("http://47.103.45.149:5000").post(body).build()
+        val client = OkHttpClient()
+        client.newCall(request).execute().use {
+            val r = it.body!!.string()
+            TODO("继续整理数据")
+        }
     }
 }
